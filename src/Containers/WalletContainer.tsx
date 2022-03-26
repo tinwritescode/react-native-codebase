c40379e0ded38d463aa83c17a6406e78921692b0
Tin Nguyen <hopthucuatin@gmail.com>
2022-03-26 11:49:24 +0700

    Add @polkadot/keyring

--- src/Containers/WalletContainer.tsx
+++ src/Containers/WalletContainer.tsx
@@ -14,6 +14,9 @@ import { useTheme } from '@/Hooks'
 import { useLazyFetchOneQuery } from '@/Services/modules/users'
 import { changeTheme, ThemeState } from '@/Store/Theme'
 
+import { Keyring } from '@polkadot/keyring'
+import { cryptoWaitReady, mnemonicGenerate } from '@polkadot/util-crypto'
+
 const WalletContainer = () => {
   const { t } = useTranslation()
   const { Common, Fonts, Gutters, Layout } = useTheme()
@@ -29,8 +32,26 @@ const WalletContainer = () => {
     fetchOne(userId)
   }, [fetchOne, userId])
 
+  useEffect(() => {
+    // cryptoWaitReady().then(() => {
+    //   const keyring = new Keyring({ type: 'sr25519' })
+    //   const mnemonic = mnemonicGenerate()
+    //   keyring.addFromMnemonic(mnemonic)
+    //   console.log(keyring.getPair('Alice').address)
+    // })
+  }, [])
+
   const onChangeTheme = ({ theme, darkMode }: Partial<ThemeState>) => {
     dispatch(changeTheme({ theme, darkMode }))
+  }
+
+  const createWalletHandler = async () => {
+    const key = new Keyring({ type: 'sr25519', ss58Format: 2 })
+    const mnemonic = mnemonicGenerate()
+
+    console.log(mnemonic)
+    key.addFromMnemonic(mnemonic)
+    console.log(key.getPublicKeys())
   }
 
   return (
@@ -44,7 +65,12 @@ const WalletContainer = () => {
     >
       {/*Text  */}
       <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Text style={Fonts.textRegular}>Wallet view</Text>
+        <TouchableOpacity
+          onPress={createWalletHandler}
+          style={[Common.backgroundPrimary, Common.button.rounded]}
+        >
+          <Text style={Fonts.textRegular}>Create wallet</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   )
